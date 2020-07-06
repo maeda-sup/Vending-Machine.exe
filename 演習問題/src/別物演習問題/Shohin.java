dccced94f98a498c86e88d8bd978d30050a69a96
fujisawa <fujisawa@R100394.supply-net.local>
2020-07-06 17:16:38 +0900

    インターフェース導入版

---
+++ 演習問題/src/別物演習問題/Shohin.java
@@ -0,0 +1,10 @@
+package 別物演習問題;
+
+public interface Shohin {
+
+	String getname();
+	int getprice();
+	String getdetail();
+	String getcomment();
+
+}
---
+++ 演習問題/src/別物演習問題/Tea.java
@@ -0,0 +1,42 @@
+package 別物演習問題;
+
+public class Tea implements Shohin {
+
+	//	フィールド変数
+	private String name;
+	private int price = 150;
+	private String detail = "英国紳士の嗜み";
+	private String comment = "英国面を感じる" ;
+
+	public Tea(String name) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.name = name;
+	}
+
+	@Override
+	public String getname() {
+		// TODO 自動生成されたメソッド・スタブ
+		return "|"+ name +"|"+ price + "円" ;
+	}
+
+	@Override
+	public int getprice() {
+		// TODO 自動生成されたメソッド・スタブ
+		return price;
+	}
+
+	@Override
+	public String getdetail() {
+		// TODO 自動生成されたメソッド・スタブ
+		return detail;
+	}
+
+	@Override
+	public String getcomment() {
+		// TODO 自動生成されたメソッド・スタブ
+		return comment;
+	}
+
+
+
+}
