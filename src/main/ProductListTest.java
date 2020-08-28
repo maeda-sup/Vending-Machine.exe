6809a31dc5af8edab21822aed59003a21f62ca82
maeda <maeda@P100398.supply-net.local>
2020-08-28 13:39:08 +0900

    無駄なクラスを削除

---
+++ src/main/ProductListTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductListTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
